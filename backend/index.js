eb59cfd9a7c2a98973169502d34bd045095702f9
va1an <valan95@gmail.com>
2025-12-10 09:52:16 +0530

    cors issue fix

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,6 @@ const allowedOrigins = [
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.options("*", cors());
